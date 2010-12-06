bf054b303da6d8e5a4ccedb418c49a88b19cf31a
Subbu Allamaraju <subbu@subbu.org>
2010-12-06 12:41:28 -0800

    Add an express/ejs example

---
+++ examples/express/ejs/app.js
@@ -0,0 +1,25 @@
+var sys = require('sys'),
+  express = require('express'),
+  resource = require('resource-router');
+
+var app = express.createServer();
+app.set('views', __dirname + '/views');
+
+// resource-router
+function main(app) {
+  app.resource('/', {
+    'get' : function(req, res) {
+      res.render('items.ejs', {items: [
+            { name: 'item1', description: 'This is item 1'},
+            { name: 'item2', description: 'This is item 2' },
+            { name: 'item3', email: 'This is item 3' }]
+      });
+    }
+  });
+}
+
+// Fire up
+var server = express.createServer();
+server.use(resource(main));
+server.listen(3000);
+console.log('Connect server listening on port 3000');
