1d297431441e043092fe6a0656da627d0dc99fb5
Subbu Allamaraju <subbu@subbu.org>
2010-12-08 14:16:11 -0800

    More tests

--- tests/tests.js
+++ tests/tests.js
@@ -71,10 +71,116 @@ module.exports = {
         }
       });
     }
+
     server.use(resource(main));
 
     assert.response(server,
     { url: '/', method: 'PUT'},
-    { status: 405} );
+    { status: 405});
+  },
+
+  'test routes' : function() {
+    var server = connect.createServer();
+
+    var users = [
+      { name: 'tj' },
+      { name: 'tim' }
+    ];
+
+    function user(app) {
+      app.resource('/.:format?', {
+        'get' : function(req, res, next) {
+          switch (req.params.format) {
+            case 'json':
+              var body = JSON.stringify(users);
+              res.writeHead(200, {
+                'Content-Type': 'application/json',
+                'Content-Length': body.length
+              });
+              res.end(body);
+              break;
+            default:
+              var body = '<ul>'
+                + users.map(
+                           function(user) {
+                             return '<li>' + user.name + '</li>';
+                           }).join('\n')
+                + '</ul>';
+              res.writeHead(200, {
+                'Content-Type': 'text/html',
+                'Content-Length': body.length
+              });
+              res.end(body);
+          }
+        }
+      });
+
+      app.resource('/:id.:format', {
+        'get' : function(req, res, next) {
+          var user = users[req.params.id];
+          if (user && req.params.format === 'json') {
+            user = JSON.stringify(user);
+            res.writeHead(200, {
+              'Content-Type': 'application/json',
+              'Content-Length': user.length
+            });
+            res.end(user);
+          }
+          else {
+            // When true is passed, provide control
+            // back to middleware, skipping route
+            // match attemps
+            next(true);
+          }
+        }
+      })
+
+      app.resource('/:id/:op?', {
+        'get' : function(req, res) {
+          if(users[req.params.id]) {
+            var body = users[req.params.id]
+              ? users[req.params.id].name
+              : 'User ' + req.params.id + ' does not exist';
+            body = (req.params.op || 'view') + 'ing ' + body;
+            res.writeHead(200, {
+              'Content-Type': 'text/html',
+              'Content-Length': body.length
+            });
+            res.end(body, 'utf8');
+          }
+          else {
+            res.writeHead(404);
+            res.end('Not found', 'utf8');
+          }
+        }
+      })
+    }
+
+    server.use('/users', resource(user));
+
+    assert.response(server,
+    { url: '/users', method: 'GET'},
+    { status: 200,
+      headers: {'Content-Type': 'text/html'}});
+
+    var st= JSON.stringify(users);
+
+    assert.response(server,
+    { url: '/users.json', method: 'GET'},
+      function(res) {
+        assert.equal(200, res.statusCode);
+        assert.equal('application/json', res.headers['content-type']);
+        assert.deepEqual(users, JSON.parse(res.body));
+      });
+
+    assert.response(server,
+    { url: '/users/1.json', method: 'GET'},
+    { status: 200,
+      headers: {'Content-Type': 'application/json'}});
+
+    assert.response(server,
+    { url: '/users/blah', method: 'GET'},
+    { status: 404});
+
   }
 };
